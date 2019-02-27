829d72fee900d6e578a767daa9e938ea96439ae2
“Brett <bretttewell@gmail.com>
2019-02-27 12:23:06 -0600

    whiteboard exercise array to hash(corrected code)

--- whiteboarding-7.rb
+++ whiteboarding-7.rb
@@ -1,11 +1,9 @@
 # Write a function that accepts an array and returns it as a hash. For example, [“a”, “b”, “c] should turn into {0 => “a”, 1 => “b”, 2 => “c”]
 
 def hasher_method(input_array)
-  i = 0
   new_hash = {}
   input_array.length.times do |element|
-    p"#{i} => #{input_array[element]}"
-    i += 1 
+    new_hash[element] = input_array[element] 
   end
   return new_hash 
 end
