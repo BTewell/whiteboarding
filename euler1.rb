4a142b8f5c3eaf1520a0ca70b63fca96845b1b4f
“Brett <bretttewell@gmail.com>
2019-03-04 21:31:03 -0600

    Euler exercise problem 1 - Each Loop solution

---
+++ euler1.rb
@@ -0,0 +1,14 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+#array of numbers, 1-10
+#identify which numbers are divisible by 3 and 5
+#add those identified numbers together
+sum = 0
+(1..1000).each do | n|
+    if n % 3 == 0 || n % 5 ==0
+      sum += n
+      p n
+    end
+end
+p sum
---
+++ whiteboarding-8.rb
@@ -0,0 +1,24 @@
+# def array_sorter(array)
+# new_array = []
+# index = 0
+#   array.each do
+#     new_array << array[index]
+#     index += 2
+#     p index
+#     p new_array
+#   end
+#   return new_array
+# end
+
+# p array_sorter(["a", "b", "c", "d", "e", "f", "g"])
+def awesome_sauce_machine(array)
+  awesome_array = []
+  index = 0
+    array.length.times do
+      awesome_array << array[index] + ", Awesomesauce!"
+      index += 1
+    end
+  return awesome_array
+end
+
+p awesome_sauce_machine(["a", "b", "c", "d", "e"])
